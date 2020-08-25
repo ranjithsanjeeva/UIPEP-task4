4b94d9c5e62b2e14a1db283a2f9205c372c86138
Ranjith <ranjithsanjeeva@gmail.com>
2020-08-25 09:51:14 +0530

    encryption i added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -62,6 +62,7 @@ export class LoginComponent implements OnInit {
             .pipe(first())
             .subscribe(
                 data => {
+                    console.log(data)
                     this.router.navigate([this.returnUrl]);
                 },
                 error => {
