57b8d127ac1807881a3d554d1b2ba8cbe39c1be1
Ranjith <ranjithsanjeeva@gmail.com>
2020-08-31 20:40:02 +0530

    challenge 5 started

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient,HttpHeaders, HttpParams } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { AuthenticationService } from '../_services';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent implements OnInit {
     //users = [];
+    registerForm: FormGroup;
     user1;
     user2;
     private currentUserSubject: BehaviorSubject<any>;
    
 
     constructor(
+        private formBuilder: FormBuilder,
         private router: Router,
         private authenticationService: AuthenticationService,
         private http: HttpClient,
@@ -23,6 +27,12 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
+
+        
+            this.registerForm = this.formBuilder.group({
+                message: ['', Validators.required]
+            });
+        
         // console.log(this.currentUserSubject.getValue())
         // console.log("king")
         const headers = new HttpHeaders().append('Content-Type', 'application/json').append('token',this.currentUserSubject.getValue());
@@ -37,14 +47,14 @@ export class HomeComponent implements OnInit {
     //    }, error => {
     //         console.log(error)
     //    })).subscribe();;
-    this.http.get(`http://localhost:3000/findUsers`, { headers }).subscribe(data => {
-        console.log(JSON.stringify(data))
-        this.user1=data
-      },
-      error => {
-                console.log(error)
-            })
-    
+        this.http.get(`http://localhost:3000/findUsers`, { headers }).subscribe(data => {
+            console.log(JSON.stringify(data))
+            this.user1=data
+        },
+        error => {
+            console.log(error)
+        })
+        
         this.displayUser();
     }
     deleteUser(id: string) {
@@ -54,7 +64,18 @@ export class HomeComponent implements OnInit {
             
         });
     }
+    
+    onSubmit() {
+        console.log(this.registerForm.value.message)
+        console.log(this.user1.username)
 
+        const headers = new HttpHeaders().append('Content-Type', 'application/json');
+
+        const params = new HttpParams().append('message', this.registerForm.value.message).append('username', this.user1.username);
+        this.http.get(`http://localhost:3000/update`, { headers, params }).subscribe(data => {
+            console.log(JSON.stringify(data))
+          })
+    }
     // deleteUsers(id: string) {
     //     this.http.delete(`http://localhost:3000/`+ id).subscribe(data => {
     //         // this.authenticationService.logout();
